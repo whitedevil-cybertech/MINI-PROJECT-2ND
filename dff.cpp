9124801944b05f4dcbbe70a64fdb42595ba63747
whitedevil-cybertech <mramitmahato001@gmail.com>
2025-03-10 17:20:32 +0530

    ..

---
+++ dff.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int num1, num2, sum;
+
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+
+    sum = num1 + num2;
+
+    std::cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
